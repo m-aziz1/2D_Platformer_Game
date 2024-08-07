1a1fa07f28fa008aa05a2eb19abb845e3b20ecba
Muhammad <m.ihsanaziz1@gmail.com>
2024-08-07 01:53:55 -0600

    Improve player movement and map input

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -1,28 +1,40 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -450.0 # CHANGED 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var sprite_2d = $Sprite2D #ADDED - creates reference for sprite
 
 func _physics_process(delta):
+	# ADDED - Animations
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "default"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		sprite_2d.animation = "jumping"
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 12) # CHANGED - last parameter
 
 	move_and_slide()
+	
+	# ADDED - Flip character
+	if Input.is_action_just_pressed('left'):
+		sprite_2d.flip_h = true
+	if Input.is_action_just_pressed('right'):
+		sprite_2d.flip_h = false
